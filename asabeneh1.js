2b7793c2c8bb00a54892be6e2105cab4c355415d
Lamarr <tonyleo2019@outlook.com>
2021-12-14 17:01:58 -0500

    swopp2 done

---
+++ asabeneh1.js
@@ -0,0 +1,13 @@
+//comments can make code readable
+//Welcome to 30DaysOfJavaScript
+
+/*
+comments can make code readable, easy to reuse and informative
+*/
+let name = "Montana";
+let age = 26;
+let isMarried = true;
+let job ;
+
+console.log(age, isMarried, job, career,);
+
